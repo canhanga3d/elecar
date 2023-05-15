3866ec904a7c96ea30e5ba5738bd5fd10ec73cc9
Domingos Canhanga <canhanga96@gmail.com>
2023-05-15 10:44:20 +0100

    first commit

---
+++ src/assets/ts/main.js
@@ -0,0 +1,53 @@
+import 'remixicon/fonts/remixicon.css'
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== POPULAR SWIPER ===============*/
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
